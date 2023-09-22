220b56da404f68ef46fa7fcbbd39abafc42876fd
chux <chuxoguh@gmail.com>
2023-09-22 11:53:54 +0100

    Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { auth } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import Image from "../assets/image5.jpg";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -14,19 +15,11 @@ const Login = () => {
     e.preventDefault();
 
     setLoading(true);
-
     try {
-      await signInWithEmailAndPassword(auth, email, password).then(
-        (userCred) => {
-          console.log(userCred);
-          navigate("/gallery");
-        }
-      );
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/gallery");
     } catch (error) {
-      setError("ABeg Try enter the correct login Details Again", error);
-      console.error((error) => {
-        console.log(error);
-      });
+      setError("Invalid email or password. Please try again.");
       setTimeout(() => {
         setError(null);
       }, 2000);
@@ -36,58 +29,58 @@ const Login = () => {
   };
 
   return (
-    <div className="max-w-sm mx-auto mt-10 p-6 bg-white rounded shadow-md">
-      <h1>Email: user@example.com</h1>
-      <h1>Password: 1Password</h1>
-      <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
-      <form onSubmit={handleLogin}>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="max-w-md w-full p-6 bg-white rounded shadow-md">
+        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            disabled={loading}
           >
-            Email:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-          disabled={loading}
-        >
-          {loading ? "Logging in" : "Login"}
-        </button>
-      </form>
-      {error && (
-        <div className="mt-4 bg-red-800 text-white font-bold py-2 px-4 rounded">
-          {error}
-        </div>
-      )}
+            {loading ? "Logging in..." : "Login"}
+          </button>
+        </form>
+        {error && (
+          <div className="mt-4 bg-red-100 text-red-700 font-bold py-2 px-4 rounded text-center">
+            {error}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
